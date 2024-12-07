9b9532ab5c54d917f71d6275bc62d02960dd208f
Dzmitry Nikalayeu <dimitra86@list.ru>
2024-12-07 13:37:23 +0300

    feat: 33 passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -328,8 +328,16 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  const balance = 0;
+  if (arr.length === 0) {
+    return balance;
+  }
+
+  return arr.reduce(
+    (balance2, [income, expense]) => balance2 + (income - expense),
+    0
+  );
 }
 
 /**

39d34d4340a6eda129c92e09a784589fa1950321
Dzmitry Nikalayeu <dimitra86@list.ru>
2024-12-07 19:28:18 +0300

    feat: 39 passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -573,8 +573,24 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const arrLen = arr.length;
+  let part1;
+  let part2;
+
+  if (n > 0) {
+    part2 = arr.slice(0, arrLen - n);
+    part1 = arr.slice(arrLen - n);
+  } else if (n === 0) {
+    return arr;
+  } else {
+    // n += arrLen;
+    part2 = arr.slice(0, arrLen - (n + arrLen));
+    part1 = arr.slice(arrLen - (n + arrLen));
+  }
+
+  const newArr = part1.concat(part2);
+  return newArr;
 }
 
 /**

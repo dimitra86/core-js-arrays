b0f0cde14a52285a5b113630076617622e5e8cfe
Dzmitry Nikalayeu <dimitra86@list.ru>
2024-12-08 23:06:08 +0300

    feat: 46 passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -454,8 +454,13 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  if (n <= 0) {
+    return null;
+  }
+  return Array.from({ length: n }, (_, i) =>
+    Array.from({ length: n }, (__, j) => (i === j ? 1 : 0))
+  );
 }
 
 /**
